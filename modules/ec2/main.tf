826c578cac022d2508236f2f2cd286638587be96
tmoshe <64310013+tmoshe@users.noreply.github.com>
2023-07-23 21:14:51 +0300

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,21 +1,3 @@
-/*
-resource "aws_instance" "ec2_instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
-  key_name      = var.key_name
-  tags = {
-    Name = var.instance_name
-  }
-
-
-
-  user_data = var.user_data
-
-}
-*/
-
 
 resource "aws_instance" "public_instance" {
   ami           = var.ami_id
